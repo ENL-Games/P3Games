d8923a76b343df236b19a6abd6b2244129f09eda
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-07-08 16:27:17 +0900

    [NumberPang] ##프로젝트 생성 + bg 이미지 출력

--- NumberPang/src/scenes/GameScene.ts
+++ NumberPang/src/scenes/GameScene.ts
@@ -1,14 +1,22 @@
 import Phaser from 'phaser';
 
-export default class GameScene extends Phaser.Scene
-{
+export default class GameScene extends Phaser.Scene {
+
+   private _canvasWidth: number = 0;
+   private _canvasHeight: number = 0;
+
    constructor() {
-      super('GameScene')
+      super({ key: 'GameScene' })
    }
 
    preload() {
+      this.load.image('bg', 'assets/bg.png');
    }
 
    create() {
+      this._canvasWidth = this.sys.canvas.width;
+      this._canvasHeight = this.sys.canvas.height;
+
+      this.add.image(this._canvasWidth / 2, this._canvasHeight / 2, 'bg');
    }
 }
