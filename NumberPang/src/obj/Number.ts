694d35f7c6876f03bb6430968fb615e6efd82313
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-07-08 17:53:45 +0900

    [NumberPang] ##프로젝트 생성 + obj/Number  - NewType.Position 적용

--- NumberPang/src/obj/Number.ts
+++ NumberPang/src/obj/Number.ts
@@ -1,9 +1,12 @@
 import Phaser from 'phaser';
+import { Position } from '~/NewType';
 import GameScene from '~/scenes/GameScene';
 
 export default class Number extends Phaser.GameObjects.Graphics{
 
    private _num: number = 0;
+
+   POSITION: Position = { x:0, y:0 };
 
    private _outline!: Phaser.GameObjects.Arc;
    private _text!: Phaser.GameObjects.Text;
@@ -12,11 +15,13 @@ export default class Number extends Phaser.GameObjects.Graphics{
       super(__scene);
    }
 
-   Setup(__number: number, __x: number, __y: number) {
+   Setup(__number: number, __position: Position) {
 
       this._num = __number;
+      this.POSITION.x = __position.x;
+      this.POSITION.y = __position.y;
 
-      this._outline = this.scene.add.circle(__x, __y, 24);
+      this._outline = this.scene.add.circle(__position.x, __position.y, 24);
       {
          this._outline.setStrokeStyle(4, 0xffffff);
 
@@ -25,7 +30,7 @@ export default class Number extends Phaser.GameObjects.Graphics{
          });//이벤트 처리
       }
 
-      this._text = this.scene.add.text(__x, __y, __number.toString());
+      this._text = this.scene.add.text(__position.x, __position.y, __number.toString());
       {
          this._text.setOrigin(0.5, 0.5);
          this._text.setStyle({
