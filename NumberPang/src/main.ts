115fd32e121b316d3c5e4fa927010c79eca9bd84
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-07-08 16:01:33 +0900

    [NumberPang] ##프로젝트 생성 + GameScene 추가

--- NumberPang/src/main.ts
+++ NumberPang/src/main.ts
@@ -1,18 +1,24 @@
 import Phaser from 'phaser'
 
-import HelloWorldScene from './scenes/HelloWorldScene'
+import GameScene from './scenes/GameScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
+	backgroundColor: '#4e4e4e',
+
+	scale: {
+		mode: Phaser.Scale.NONE,
+		width: 1280,
+		height: 720,
 	},
-	scene: [HelloWorldScene]
+
+	// physics: {
+	// 	default: 'arcade',
+	// 	arcade: {
+	// 		gravity: { y: 200 }
+	// 	}
+	// },
+	scene: [GameScene]
 }
 
 export default new Phaser.Game(config)
