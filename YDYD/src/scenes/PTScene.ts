0c9e01fca8d6cc210e810177b1c5023e0bc325df
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-07-30 00:41:33 +0900

    [YDYD] ## + PT Scene 추가  - 돌아가기 버튼 추가

--- YDYD/src/scenes/PTScene.ts
+++ YDYD/src/scenes/PTScene.ts
@@ -4,6 +4,8 @@ export default class PTScene extends Phaser.Scene {
 
    _bgs!: Phaser.GameObjects.Image[];
    _indexBG: number = 0;
+
+   private _txtBtn_Back!: Phaser.GameObjects.Text;
    
    constructor() {
       super({ key: 'PTScene' })
@@ -31,7 +33,16 @@ export default class PTScene extends Phaser.Scene {
          this.Tapped_Screen();
       });//이벤트 처리
 
-      // this.input.on('pointerdown', this.Tapped_Screen);
+      this._txtBtn_Back = this.add.text(canvasWidth / 2, canvasHeight - 60, "돌아가기");
+      {
+         this._txtBtn_Back.setOrigin(0.5, 0.5);
+         this._txtBtn_Back.setStyle({
+            font: "bold 80px Arial"
+         });
+         this._txtBtn_Back.setColor(`#ffff00`);
+         this._txtBtn_Back.setStroke(`#ffffff`, 8);
+         this._txtBtn_Back.setVisible(false);
+      }
    }
 
    Tapped_Screen() {
@@ -42,6 +53,12 @@ export default class PTScene extends Phaser.Scene {
 
       if(this._bgs.length <= this._indexBG + 1) {
          this.input.off('pointerdown');
+
+         this._txtBtn_Back.setVisible(true);
+         this._txtBtn_Back.setInteractive().on('pointerdown', (pointer, localX, localY) => {
+            // console.log("_txtBtn_Back");
+            this.scene.switch(`IntroScene`);
+         });//이벤트 처리
       }
    }
 
