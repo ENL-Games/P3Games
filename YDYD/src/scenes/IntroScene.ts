a9bcf8ea6175a610efc8c169294043a555e6dda1
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-07-29 22:29:31 +0900

    [YDYD] ## + Intro Scene  - 타이틀 그라디언트

--- YDYD/src/scenes/IntroScene.ts
+++ YDYD/src/scenes/IntroScene.ts
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 
 export default class IntroScene extends Phaser.Scene {
+
+   private _text_Title!: Phaser.GameObjects.Text;
+
    constructor() {
       super({ key: 'IntroScene' })
    }
@@ -14,14 +17,24 @@ export default class IntroScene extends Phaser.Scene {
 
       this.add.image(canvasWidth / 2, canvasHeight / 2, 'bg-intro');
 
-      let title = this.add.text(canvasWidth / 2, 200, `영도 2072`);
+      this._text_Title = this.add.text(canvasWidth / 2, 200, `2072 영도`);
       {
-         title.setOrigin(0.5, 0.5);
-         title.setStroke(`#000000`, 8);
-         title.setColor(`#00ff00`);
-         title.setStyle({
+         this._text_Title.setOrigin(0.5, 0.5);
+         this._text_Title.setStyle({
             font: "bold 100px Arial"
          });
+
+         this._text_Title.setColor(`#00ff00`);
+
+         const gradient = this._text_Title.context.createLinearGradient(0, 0, 0, this._text_Title.height);
+         {
+            gradient.addColorStop(0, '#ff974b');
+            gradient.addColorStop(.5, '#fff435');
+            gradient.addColorStop(1, '#ff9543');
+         }
+         this._text_Title.setFill(gradient);
+
+         // this._text_Title.setVisible(false);
       }
    }
 }
