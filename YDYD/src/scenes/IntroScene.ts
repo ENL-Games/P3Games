7f36f7f439194e4660b99e627ea5bb8e043d46af
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-07-29 22:01:40 +0900

    [YDYD] ## + Intro Scene  - 타이틀 추가

--- YDYD/src/scenes/IntroScene.ts
+++ YDYD/src/scenes/IntroScene.ts
@@ -13,5 +13,15 @@ export default class IntroScene extends Phaser.Scene {
       let canvasHeight = this.sys.canvas.height;
 
       this.add.image(canvasWidth / 2, canvasHeight / 2, 'bg-intro');
+
+      let title = this.add.text(canvasWidth / 2, 200, `영도 2072`);
+      {
+         title.setOrigin(0.5, 0.5);
+         title.setStroke(`#000000`, 8);
+         title.setColor(`#00ff00`);
+         title.setStyle({
+            font: "bold 100px Arial"
+         });
+      }
    }
 }
