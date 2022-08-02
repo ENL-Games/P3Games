7cc5bc843d5423a4fc1b8a43182e77447b41f992
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-02 18:41:33 +0900

    [YDYD] ## + Game Scene-GameSelector-GameSelectBox: 버튼 이벤트 처리  - over / out  - down  - 박스 2개

--- YDYD/src/scenes/GameSelectBox.ts
+++ YDYD/src/scenes/GameSelectBox.ts
@@ -1,23 +1,25 @@
 import Phaser from "phaser";
+import { GameSelector } from "./GameSelector";
 
 export class GameSelectBox extends Phaser.GameObjects.Container {
 
-   _box!: Phaser.GameObjects.Rectangle;
+   _boxOutline!: Phaser.GameObjects.Rectangle;
    _text!: Phaser.GameObjects.Text;
 
-   constructor(__scene) {
+   constructor(__scene, __selector: GameSelector, __index: number) {
       super(__scene);
       __scene.add.existing(this);
 
-      this._box = this.scene.add.rectangle(0, 0, 720, 60
+      let box = this.scene.add.rectangle(0, 0, 720, 60
          , 0x000000);
       {
-         this.Add_ContainerItem(this._box);
-         this._box.setAlpha(0.75);
+         this.Add_ContainerItem(box);
+         box.setAlpha(0.75);
 
-         let boxOutline = this.scene.add.rectangle(0, 0, 720, 60);
-         this.Add_ContainerItem(boxOutline);
-         boxOutline.setStrokeStyle(3, 0xffff00);
+         this._boxOutline = this.scene.add.rectangle(0, 0, 720, 60);
+         this.Add_ContainerItem(this._boxOutline);
+         // this._boxOutline.setStrokeStyle(3, 0xffff00);
+         this.Set_OutlineColor();
 
          this._text = this.scene.add.text(0, 0, "");
          this.Add_ContainerItem(this._text);
@@ -25,6 +27,27 @@ export class GameSelectBox extends Phaser.GameObjects.Container {
          this._text.setColor(`#ffffff`);
          this._text.setFontSize(22);
       }
+
+      this._boxOutline.setInteractive().on('pointerover', (pointer, localX, localY) => {
+         // console.log("over: " + this._text.text);
+         this.Set_OutlineColor('pointerover');
+      });//이벤트 처리
+      this._boxOutline.setInteractive().on('pointerout', (pointer, localX, localY) => {
+         // console.log("out: " + this._text.text);
+         this.Set_OutlineColor('pointerout');
+      });//이벤트 처리
+
+      this._boxOutline.setInteractive().on('pointerdown', (pointer, localX, localY) => {
+         // console.log('down: ' + this._text.text);
+         __selector.OnClick_SelectBox(__index);
+      });//이벤트 처리
+   }
+
+   private Set_OutlineColor(__event: string = "") {
+      let color = 0xffff00;
+      if('pointerover' == __event)
+         color = 0x00ffff;
+      this._boxOutline.setStrokeStyle(3, color);
    }
 
    Set_Text(__text: string) {
--- YDYD/src/scenes/GameSelector.ts
+++ YDYD/src/scenes/GameSelector.ts
@@ -1,14 +1,24 @@
 import Phaser from "phaser";
+import { GameSelectBox } from "./GameSelectBox";
 
 export class GameSelector extends Phaser.GameObjects.Container {
    constructor(__scene) {
       super(__scene);
       __scene.add.existing(this);
 
-      let box = this.scene.add.rectangle(0, 0, 100, 100, 0xffff00);
-      {
-         this.Add_ContainerItem(box);
-      }
+      let box = new GameSelectBox(__scene, this, 0);
+      this.Add_ContainerItem(box);
+      box.setPosition(this.scene.sys.canvas.width / 2, this.scene.sys.canvas.height / 2 - 60);
+      box.Set_Text("1. 참자!!");
+
+      box = new GameSelectBox(__scene, this, 1);
+      this.Add_ContainerItem(box);
+      box.setPosition(this.scene.sys.canvas.width / 2, this.scene.sys.canvas.height / 2 + 60);
+      box.Set_Text("2. 공격!!");
+   }
+
+   OnClick_SelectBox(__index: number) {
+      console.log(`OnClick_SelectBox(index= ${__index})`);
    }
 
    private Add_ContainerItem(__item: Phaser.GameObjects.GameObject) {
