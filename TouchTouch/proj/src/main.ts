c2f4a30daa77926bebed1885f67e6f1d6f3dcd8e
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-03 18:24:45 +0900

    [TouchTouch - proj] ## + assets 추가하고 PreLoader

--- TouchTouch/proj/src/main.ts
+++ TouchTouch/proj/src/main.ts
@@ -1,18 +1,24 @@
 import Phaser from 'phaser'
-
-import HelloWorldScene from './scenes/HelloWorldScene'
+import PreLoader from './scenes/PreLoader'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
+	backgroundColor: '#4e4e4e',
+
+	scale: {
+		mode: Phaser.Scale.FIT,
+		width: 1280,
+		height: 720,
 	},
-	scene: [HelloWorldScene]
+
+	// physics: {
+	// 	default: 'arcade',
+	// 	arcade: {
+	// 		gravity: { y: 200 }
+	// 	}
+	// },
+	scene: [ PreLoader,
+	]
 }
 
 export default new Phaser.Game(config)
---
+++ TouchTouch/proj/src/scenes/PreLoader.ts
@@ -0,0 +1,19 @@
+import Phaser from "phaser";
+
+export default class PreLoader extends Phaser.Scene {
+   constructor() {
+      super("PreLoader");
+   }
+
+   preload() {
+      this.load.image('block-boss', 'assets/block-boss.png');
+      this.load.image('block-boss-cracked', 'assets/block-boss-cracked.png');
+      this.load.image('block-cat', 'assets/block-cat.png');
+      this.load.image('block-cat-cracked', 'assets/block-cat-cracked.png');
+      this.load.image('block-gob', 'assets/block-gob.png');
+      this.load.image('block-boss-cracked', 'assets/block-boss-cracked.png');
+   }
+
+   create() {
+   }
+}
