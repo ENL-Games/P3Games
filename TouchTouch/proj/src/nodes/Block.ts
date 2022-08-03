1733f86ad5d96e6fc46186234425de5b63b9cfc7
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-03 23:48:44 +0900

    [TouchTouch - proj] ## + nodes/Block: 이미지 변경  - 처리 추가  - ※ 테스트용 터치 이벤트 추가

--- TouchTouch/proj/src/nodes/Block.ts
+++ TouchTouch/proj/src/nodes/Block.ts
@@ -5,11 +5,43 @@ export default class Block extends zNode {
 
    _baseImage!: Phaser.GameObjects.Image;
 
-   constructor(__scene) {
+   _index: number = 0;
+   _isCracked: boolean = false;
+
+   constructor(__scene, __index: number) {
       super(__scene);
       __scene.add.existing(this);
 
       this._baseImage = __scene.add.image(0, 0, `block-boss`);
       this.Add_ContainerItem(this._baseImage);
+
+      this._index = __index;
+      this._isCracked = false;
+
+      this.Choose_BaseImage();
+
+      {//TEST
+         this._baseImage.setInteractive().on('pointerdown', (pointer, localX, localY) => {
+            this.Crack();
+         });//이벤트 처리
+      }
+   }
+
+   Choose_BaseImage() {
+      let texture: string = BaseImage_Texture[this._index];
+      if(this._isCracked) {
+         texture += `-cracked`;
+      }
+      // console.log(texture);
+      this._baseImage.setTexture(texture);
+   }
+
+   Crack() {
+      this._isCracked = !this._isCracked;
+      this.Choose_BaseImage();
    }
 }
+
+const BaseImage_Texture = [
+   `block-boss`, `block-gob`, `block-cat`
+];
