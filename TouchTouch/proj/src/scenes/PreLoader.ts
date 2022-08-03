a0660d21b4c5bb4d3e3af797b83b51101c6f342a
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-03 23:47:42 +0900

    [TouchTouch - proj] ## + PreLoader: Block 이미지  - 로드 오류 수정

--- TouchTouch/proj/src/scenes/PreLoader.ts
+++ TouchTouch/proj/src/scenes/PreLoader.ts
@@ -13,10 +13,10 @@ export default class PreLoader extends Phaser.Scene {
 
       this.load.image('block-boss', 'assets/block-boss.png');
       this.load.image('block-boss-cracked', 'assets/block-boss-cracked.png');
+      this.load.image('block-gob', 'assets/block-gob.png');
+      this.load.image('block-gob-cracked', 'assets/block-gob-cracked.png');
       this.load.image('block-cat', 'assets/block-cat.png');
       this.load.image('block-cat-cracked', 'assets/block-cat-cracked.png');
-      this.load.image('block-gob', 'assets/block-gob.png');
-      this.load.image('block-boss-cracked', 'assets/block-boss-cracked.png');
    }
 
    create() {
