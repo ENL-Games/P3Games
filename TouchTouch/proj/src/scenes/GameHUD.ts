b29a0434ae84a9c285d4d448ce4d438e29da6df3
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-12 22:19:07 +0900

    [TouchTouch - proj] ## + scenes/GameHUD: number 타이머 삭제

--- TouchTouch/proj/src/scenes/GameHUD.ts
+++ TouchTouch/proj/src/scenes/GameHUD.ts
@@ -4,7 +4,6 @@ import GameScene from './GameScene';
 
 class GameHUD extends Phaser.Scene {
    
-   _text_Time!: Phaser.GameObjects.Text;
    _text_Score!: Phaser.GameObjects.Text;
    _score: number = 0;   
 
@@ -30,15 +29,6 @@ class GameHUD extends Phaser.Scene {
    Make_UI() {
       let canvasWidth = this.sys.canvas.width;
       let canvasHeight = this.sys.canvas.height;
-
-      this._text_Time = this.add.text(canvasWidth / 2, 52, `0`);
-      {
-         this._text_Time.setOrigin(0.5, 0.5);
-         this._text_Time.setStyle({
-            font: "bold 40px Arial",
-         });
-         this._text_Time.setColor(`#00ff00`);
-      }
 
       this._text_Score = this.add.text(canvasWidth / 2, 118, `SCORE: ${0}`);
       {
@@ -159,7 +149,7 @@ class GameHUD extends Phaser.Scene {
       //    console.log(this._sec);
       // }
       // console.log(sec);
-      
+
       // console.log(`remain: ${this._limitTime} - ${t} = ${remain}`);
       this.Update_Gauge(ratio);
    }
