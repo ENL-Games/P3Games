cbc995302defaab35bbfe5b4d714ce96d4cecb72
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-12 17:32:35 +0900

    [TouchTouch - proj] ## + scenes/GameHUD: GameScene에 구현한 UI 이관

--- TouchTouch/proj/src/scenes/GameHUD.ts
+++ TouchTouch/proj/src/scenes/GameHUD.ts
@@ -1,6 +1,9 @@
 import Phaser from 'phaser';
 
 export default class GameHUD extends Phaser.Scene {
+   
+   _text_Time!: Phaser.GameObjects.Text;
+   _text_Score!: Phaser.GameObjects.Text;
 
    constructor() {
       super({ key: 'GameHUD' });
@@ -9,5 +12,24 @@ export default class GameHUD extends Phaser.Scene {
    preload() {}
 
    create() {
+      let canvasWidth = this.sys.canvas.width;
+      let canvasHeight = this.sys.canvas.height;
+
+      this._text_Time = this.add.text(canvasWidth / 2, 52, `0`);
+      {
+         this._text_Time.setOrigin(0.5, 0.5);
+         this._text_Time.setStyle({
+            font: "bold 40px Arial",
+         });
+         this._text_Time.setColor(`#00ff00`);
+      }
+
+      this._text_Score = this.add.text(canvasWidth / 2, 118, `SCORE: ${0}`);
+      {
+         this._text_Score.setOrigin(0.5, 0.5);
+         this._text_Score.setStyle({
+            font: "bold 32px Arial",
+         });
+      }
    }
 }
--- TouchTouch/proj/src/scenes/GameScene.ts
+++ TouchTouch/proj/src/scenes/GameScene.ts
@@ -4,9 +4,6 @@ import Switch from "~/nodes/Switch";
 import { Async_Pause } from "~/Utils";
 
 export default class GameScene extends Phaser.Scene {
-   _text_Time!: Phaser.GameObjects.Text;
-   _text_Score!: Phaser.GameObjects.Text;
-
    _blocks: Block[][] = [];
    _blocksCount: number[] = [0, 0, 0];
    _blocksCurrent: number[] = [0, 0, 0];
@@ -19,8 +16,8 @@ export default class GameScene extends Phaser.Scene {
    preload() {}
 
    create() {
-      this.scene.run('GameHUD');    
-      
+      this.scene.run('GameHUD');
+
       let canvasWidth = this.sys.canvas.width;
       let canvasHeight = this.sys.canvas.height;
 
@@ -28,8 +25,6 @@ export default class GameScene extends Phaser.Scene {
       
       this.Make_Switch();
       this.Regen_Blocks();
-
-      this.create_UI();
    }
 
    private Make_Switch() {
@@ -69,27 +64,6 @@ export default class GameScene extends Phaser.Scene {
          }
 
          this._blocks[kind] = items;
-      }
-   }
-
-   create_UI() {
-      let canvasWidth = this.sys.canvas.width;
-
-      this._text_Time = this.add.text(canvasWidth / 2, 52, `0`);
-      {
-         this._text_Time.setOrigin(0.5, 0.5);
-         this._text_Time.setStyle({
-            font: "bold 40px Arial",
-         });
-         this._text_Time.setColor(`#00ff00`);
-      }
-
-      this._text_Score = this.add.text(canvasWidth / 2, 118, `SCORE: ${0}`);
-      {
-         this._text_Score.setOrigin(0.5, 0.5);
-         this._text_Score.setStyle({
-            font: "bold 32px Arial",
-         });
       }
    }
 
