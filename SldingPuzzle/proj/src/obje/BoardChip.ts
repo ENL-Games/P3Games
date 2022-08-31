950eb664e6118ed5e12266a924ee4f352ab44703
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-31 21:36:54 +0900

    [SldingPuzzle - proj] ## + obje/BoardChip: 숫자기입

--- SldingPuzzle/proj/src/obje/BoardChip.ts
+++ SldingPuzzle/proj/src/obje/BoardChip.ts
@@ -21,6 +21,28 @@ export default class BoardChip extends zNode {
          // bg.setPosition(500, 500);
       }
 
+      let numberCircle_Outer = __scene.add.circle(-52, -52, 18, 0x000000);
+      {
+         this.Add_ContainerItem(numberCircle_Outer);
+         numberCircle_Outer.setAlpha(0.5);
+
+         let numberCircle_Inner = __scene.add.circle(-52, -52, 16, 0xffff00);
+         this.Add_ContainerItem(numberCircle_Inner);
+         numberCircle_Inner.setAlpha(0.5);
+
+         let txtNumber = __scene.add.text(-52, -52, `${__index + 1}`);
+         {
+            this.Add_ContainerItem(txtNumber);
+
+            txtNumber.setOrigin(0.5, 0.5);
+            txtNumber.setStyle({
+               font: "bold 20px Arial",
+            });
+            txtNumber.setColor(`#ff0000`);
+            txtNumber.setAlpha(0.5);
+         }
+      }
+
       {//TEST
          this.setInteractive().on('pointerdown', (pointer, localX, localY) => {
             console.log(`${__index}`);
