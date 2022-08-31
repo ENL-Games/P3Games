89e579b05cbfd32d203c386e02019575b7736ee2
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-31 21:24:29 +0900

    [SldingPuzzle - proj] ## + obje/Board: BoardChip 무작위

--- SldingPuzzle/proj/src/obje/Board.ts
+++ SldingPuzzle/proj/src/obje/Board.ts
@@ -17,8 +17,22 @@ export default class Board extends zNode {
 
       this.Add_ChipSheet(__scene);
 
+      this.Setup_Game(this.scene);
+   }
+
+   Setup_Game(__scene) {
+      let orders: number[] = [];
+      {//shuffle
+         for (let n = 0; n < 16; n++) {
+            orders.push(n);
+         }
+         // console.log(orders);
+         orders.sort(() => Math.random() - 0.5);
+         // console.log(orders);
+      }
+
       for(let n=0; n<16; n++) {
-         let chip = new BoardChip(__scene, n, n);
+         let chip = new BoardChip(__scene, n, orders[n], `puzzle-kkang`);
       }
    }
 
