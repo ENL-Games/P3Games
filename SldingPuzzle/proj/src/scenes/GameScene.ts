b5d6c19499e527122c1db57b5798f04058ca51a1
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-08-31 16:49:43 +0900

    [SldingPuzzle - proj] ## + GameScene: bg

--- SldingPuzzle/proj/src/scenes/GameScene.ts
+++ SldingPuzzle/proj/src/scenes/GameScene.ts
@@ -13,6 +13,14 @@ export default class GameScene extends Phaser.Scene {
         let canvasWidth = this.sys.canvas.width;
         let canvasHeight = this.sys.canvas.height;
 
+        this.add.image(canvasWidth / 2, canvasHeight / 2, `bg-game`);
+        {
+            let box = this.add.rectangle(canvasWidth / 2, canvasHeight / 2
+                , canvasWidth, canvasHeight
+                , 0x000000);
+            box.setAlpha(0.75);
+        }
+
         let thumb = new Thumbnail(this);
     }
 }
