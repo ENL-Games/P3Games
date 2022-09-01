87f41e87f54dfad2e325b1deb893692d4f85d247
coder-lloyd <86070086+coder-lloyd@users.noreply.github.com>
2022-09-01 16:59:13 +0900

    [SldingPuzzle - proj] ## + ResManager: 리소스 매니저  - 퍼즐 리소스 처리

---
+++ SldingPuzzle/proj/src/ResManager.ts
@@ -0,0 +1,26 @@
+const PuzzleKey = [
+   ``
+];
+
+class ResManager {
+   static readonly PuzzleKeys = [
+      `kkang`,
+      `club`,
+   ];
+
+   constructor() {}
+
+   static Get_Puzzle_Count(): number {
+      return ResManager.PuzzleKeys.length;
+   }
+   static Get_Puzzle_ThumbKey(__index: number): string {
+      return `thumb-${ResManager.PuzzleKeys[__index]}`;
+   }
+   static Get_Puzzle_SpriteSheet(__index: number): string {
+      return `puz-${ResManager.PuzzleKeys[__index]}`;
+   }
+}
+
+export {
+   ResManager,
+}
